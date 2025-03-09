0bff040c734599ba638ffa951c2ae6f6ef7961b6
mnwdy <mnwdykn@gmail.com>
2025-03-09 14:04:56 +0900

    0309-2

--- components/Layout.js
+++ components/Layout.js
@@ -3,8 +3,8 @@ import styles from "./layout.module.css";
 import utilStyles from "../src/styles/utils.module.css";
 import Link from "next/link";
 
-const name = "みの";
-export const siteTitle = "ブログ";
+const name = "みみみくん";
+export const siteTitle = "エンジニアになりたいなんちゃってエンジニアのブログ";
 
 export default function Layout({ children, home }) {
   return (
